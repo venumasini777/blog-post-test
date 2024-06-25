009780859d1235d61f3123f5bc29aecb40ead0fa
venumasini777 <venumadhav@Venus-Laptop.local>
2024-06-25 23:38:19 +0530

    Modify pages to correct the build

--- src/utils/renderContent.tsx
+++ src/utils/renderContent.tsx
@@ -9,22 +9,29 @@ function renderContent(content: ContentNode | SubContentNode | DocumentNode): an
     const node = content as ContentNode;
     //console.log(content);
     //console.log("hi2" + node.nodeType);
-    switch (node.nodeType) {
-        case 'paragraph':
-        return `<p>${node.content.map(renderContent).join('')}</p>`;
-        case 'heading-2':
-        return `<h2>${node.content.map(renderContent).join('')}</h2>`;
-        case 'unordered-list':
-        return `<ul>${node.content.map(renderContent).join('')}</ul>`;
-        case 'list-item':
-        return `<li>${node.content.map(renderContent).join('')}</li>`;
-        default:
-        return node.content.map(renderContent).join('');
+    if(node.nodeType){
+        switch (node.nodeType) {
+            case 'paragraph':
+            return `<p>${node.content.map(renderContent).join('')}</p>`;
+            case 'heading-2':
+            return `<h2>${node.content.map(renderContent).join('')}</h2>`;
+            case 'unordered-list':
+            return `<ul>${node.content.map(renderContent).join('')}</ul>`;
+            case 'list-item':
+            return `<li>${node.content.map(renderContent).join('')}</li>`;
+            default:
+            return node.content.map(renderContent).join('');
+        }
     }
+    return '';
+    
   }
   if((content as DocumentNode)){
     const node = content as DocumentNode;
-    return `<div>${node.content.map(renderContent).join('<br>')}</div>`
+    if(node.content){
+        return `<div>${node.content.map(renderContent).join('<br>')}</div>`
+    }
+    return '';
   }
   
 }
